d08e0e55d7a290298ecf0100ce3ce2cc86da2b9a
Ekow <edonohue8@users.noreply.github.com>
2020-10-08 16:24:23 -0700

    updated index and generateMarkdown

--- index.js
+++ index.js
@@ -1,15 +1,72 @@
+// const declarations
+const inquirer = require("inquirer");
+const fs = require("fs");
+const path = require("path");
+const generateMarkdown = require("./utils/generateMarkdown");
+
 // array of questions for user
 const questions = [
-
+    {
+        type: "input",
+        name: "github",
+        message: "Please provide your GitHub username"
+    }, {
+        type: "input",
+        name: "email",
+        message: "Please provide your e-mail address"
+    },
+    {
+        type: "input",
+        name: "title",
+        message: "What is your project's title?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please write a short description of your project"
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Please select the license your project should have",
+        default: "MIT"
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "What terminal command should be run to install dependencies?",
+        default: "npm i"
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "What terminal command should be run for testing?",
+        default: "npm test"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "What should the user know about using the repo?",
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "What should the user know about contributing to the repo?",
+    }
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
 }
 
 // function to initialize program
 function init() {
-
+    inquirer.prompt(questions)
+        .then((inquirerResponses) => {
+            console.log("README file generated...");
+            writeToFile("README.md", generateMarkdown({ ...inquirerResponses }));
+        })
 }
 
 // function call to initialize program
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,86 @@
+// function to render license badge
+function renderLicenseBadge(license) {
+  if (license !== "None") {
+    return `![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`
+  }
+  return ''
+}
+
+// function to render license link
+function renderLicenseLink(license) {
+  if (license !== "None") {
+    return (
+      `\n* [License](#license)\n`
+    )
+  }
+  return ''
+}
+
+// function to render license section
+function renderLicenseSection(license) {
+  if (license !== "None") {
+    return (
+      `## License
+
+This project is licensed under the ${license} license.`
+    )
+  }
+  return ''
+}
+
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+${renderLicenseBadge(data.license)}
+
+## Description
+
+${data.description}
+
+## Table of Contents 
+
+* [Installation](#installation)
+
+* [Usage](#usage)
+${renderLicenseLink(data.license)}
+* [Contributing](#contributing)
+
+* [Tests](#tests)
+
+* [Questions](#questions)
+
+## Installation
+
+To install necessary dependencies, run the following command:
+
+\`\`\`
+${data.installation}
+\`\`\`
+
+## Usage
+
+${data.usage}
+
+${renderLicenseSection(data.license)}
+  
+## Contributing
+
+${data.contributing}
+
+## Tests
+
+To run tests, run the following command:
+
+\`\`\`
+${data.test}
+\`\`\`
+
+## Questions
+
+If you have any questions regarding my repo, please e-mail me at ${data.email}.  Please visit [${data.github}](https://github.com/${data.github}/) to see more of my projects.
 
 `;
 }
 
+// object that's returned as result of require call
 module.exports = generateMarkdown;
